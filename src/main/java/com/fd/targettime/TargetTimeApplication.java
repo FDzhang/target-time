583da72e8bec3470d4b372963ffa07740e60dc13
zhangxinqiang <1933581361@qq.com>
2021-06-26 09:47:35 +0800

    初始化项目

---
+++ src/main/java/com/fd/targettime/TargetTimeApplication.java
@@ -0,0 +1,13 @@
+package com.fd.targettime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargetTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fd/targettime/TargetTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fd.targettime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
